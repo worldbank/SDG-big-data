3067b4240424fdc2785267eb285fc98080b9e64b
Lo <lcandeago@gmail.com>
2021-08-31 23:11:11 +0200

    checked src/analysis/stay_at_home.py

--- gps-analytics/src/analysis/stay_at_home.py
+++ gps-analytics/src/analysis/stay_at_home.py
@@ -1,15 +1,6 @@
-import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import numpy as np
-import seaborn as sns
-
-import pyspark.sql.functions as F
-from pyspark.sql.functions import col, desc, lit
-from pyspark.sql import Window
-
-spark.conf.set("spark.sql.shuffle.partitions", 1500)
+from wbgps.analysis import compute_durations_and_admins, compute_durations_normalized_by_wealth_home, \
+    compute_durations_normalized_by_wealth_home_wealth_work, output, output_hw
 
 start_baseline = '2020-02-01'
 end_baseline = '2020-03-01'
@@ -17,15 +8,13 @@ start_date = '2020-02-01'
 end_date = '2021-04-15'
 
 results_dir = '/dbfs/mnt/Geospatial/results/veraset/'
-admin_path = f'/mnt/Geospatial/admin/' # country specific admin files should be stored as "{admin_path}/{country}/admin.csv"
+admin_path = f'/mnt/Geospatial/admin/'  # country specific admin files should be stored as "{admin_path}/{country}/admin.csv"
 stop_path = '/mnt/Geospatial/results/veraset/'
-
 
 bins_wealth = [0, 0.4, 0.8, 1]
 bins_dist = [0, 0.4, 0.8, 1]
 
 countries = ['ID', 'PH', 'BR', 'CO', 'MX', 'ZA']
-
 
 c_dates = {'BR': '2021-05-16',
            'CO': '2021-05-16',
@@ -45,23 +34,29 @@ mpw = 0.2
 
 ma = 28
 
-
-labels_wealth = [x+' ('+str(int(bins_wealth[i]*100))+'%-'+str(int(bins_wealth[i+1]*100)) +'%)' for i, x in zip(range(len(bins_wealth)-1), ['Low', 'Medium', 'High'])]
-labels_dist = [x+' ('+str(int(bins_dist[i]*100))+'%-'+str(int(bins_dist[i+1]*100)) +'%)' for i, x in zip(range(len(bins_dist)-1), ['Low', 'Medium', 'High'])]
-country_capitals = {'MX': 'Mexico City', 'BR': 'São Paulo', 'CO': 'Bogota', 'PH': 'Quezon City [Manila]', 'ID': 'Jakarta', 'ZA': 'Johannesburg', 'AR': 'GBuenos Aires'}
-
+labels_wealth = [x + ' (' + str(int(bins_wealth[i] * 100)) + '%-' + str(int(bins_wealth[i + 1] * 100)) + '%)' for i, x
+                 in zip(range(len(bins_wealth) - 1), ['Low', 'Medium', 'High'])]
+labels_dist = [x + ' (' + str(int(bins_dist[i] * 100)) + '%-' + str(int(bins_dist[i + 1] * 100)) + '%)' for i, x in
+               zip(range(len(bins_dist) - 1), ['Low', 'Medium', 'High'])]
+country_capitals = {'MX': 'Mexico City', 'BR': 'São Paulo', 'CO': 'Bogota', 'PH': 'Quezon City [Manila]',
+                    'ID': 'Jakarta', 'ZA': 'Johannesburg', 'AR': 'GBuenos Aires'}
 
 ### Compute mobility metrics
 results = {}
 for country in countries:
     print(country)
 
-    admins_by_country, admins_by_metro_area, pop_metro_areas = process_admin(country,admin_path)
-    capital_geomid = admins_by_metro_area[admins_by_metro_area.metro_area_name == country_capitals[country]].geom_id.unique().tolist()
-    durations_and_admins = compute_durations_and_admins(country, c_dates[country], stop_path, activity_level=activity_level, hw=hw, ww=ww, wa=wa, mph=mph, mpw=mpw)
+    admins_by_country, admins_by_metro_area, pop_metro_areas = process_admin(country, admin_path)
+    capital_geomid = admins_by_metro_area[
+        admins_by_metro_area.metro_area_name == country_capitals[country]].geom_id.unique().tolist()
+    durations_and_admins = compute_durations_and_admins(country, c_dates[country], stop_path,
+                                                        activity_level=activity_level, hw=hw, ww=ww, wa=wa, mph=mph,
+                                                        mpw=mpw)
     durations_and_admins.cache()
-    out1 = compute_durations_normalized_by_wealth_home(durations_and_admins, admins_by_country, labels_wealth, bins_wealth)
-    out2 = compute_durations_normalized_by_wealth_home_wealth_work(durations_and_admins, admins_by_country, labels_wealth, bins_wealth)
+    out1 = compute_durations_normalized_by_wealth_home(durations_and_admins, admins_by_country, labels_wealth,
+                                                       bins_wealth)
+    out2 = compute_durations_normalized_by_wealth_home_wealth_work(durations_and_admins, admins_by_country,
+                                                                   labels_wealth, bins_wealth)
     results[(country, 't_home')] = output(out1, column='H')
     results[(country, 't_work')] = output(out1, column='W')
     results[(country, 't_other')] = output(out1, column='O')
@@ -79,4 +74,4 @@ for key, res in results.items():
     res_tmp['state'], res_tmp['measure'] = key
     res2 = res2.append(res_tmp, ignore_index=True)
 
-res2.to_csv(results_dir+'all_hw_weighted.csv')
+res2.to_csv(results_dir + 'all_hw_weighted.csv')
