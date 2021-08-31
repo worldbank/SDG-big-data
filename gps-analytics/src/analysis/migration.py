a4f01d7481fbddf326dec40e03a26ff64b974c8d
Lo <lcandeago@gmail.com>
2021-08-31 22:58:04 +0200

    fixed migration.py

--- gps-analytics/src/analysis/migration.py
+++ gps-analytics/src/analysis/migration.py
@@ -1,15 +1,5 @@
-import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import numpy as np
-import seaborn as sns
-
-import pyspark.sql.functions as F
-from pyspark.sql.functions import col, desc, lit
-from pyspark.sql import Window
-
-spark.conf.set("spark.sql.shuffle.partitions", 1500)
+from wbgps import compute_rural_migration_stats_city
 
 start_baseline = '2020-02-01'
 end_baseline = '2020-03-01'
@@ -17,7 +7,7 @@ start_date = '2020-02-01'
 end_date = '2021-04-15'
 
 results_dir = '/dbfs/mnt/Geospatial/results/veraset/'
-admin_path = f'/mnt/Geospatial/admin/' # country specific admin files should be stored as "{admin_path}/{country}/admin.csv"
+admin_path = f'/mnt/Geospatial/admin/'  # country specific admin files should be stored as "{admin_path}/{country}/admin.csv"
 stop_path = '/mnt/Geospatial/results/veraset/'
 
 bins_wealth = [0, 0.4, 0.8, 1]
@@ -47,20 +37,22 @@ ma = 28
 # Smoothing size for migrations
 mw = 28
 
-labels_wealth = [x+' ('+str(int(bins_wealth[i]*100))+'%-'+str(int(bins_wealth[i+1]*100)) +'%)' for i, x in zip(range(len(bins_wealth)-1), ['Low', 'Medium', 'High'])]
-labels_dist = [x+' ('+str(int(bins_dist[i]*100))+'%-'+str(int(bins_dist[i+1]*100)) +'%)' for i, x in zip(range(len(bins_dist)-1), ['Low', 'Medium', 'High'])]
+labels_wealth = [x + ' (' + str(int(bins_wealth[i] * 100)) + '%-' + str(int(bins_wealth[i + 1] * 100)) + '%)' for i, x
+                 in zip(range(len(bins_wealth) - 1), ['Low', 'Medium', 'High'])]
+labels_dist = [x + ' (' + str(int(bins_dist[i] * 100)) + '%-' + str(int(bins_dist[i + 1] * 100)) + '%)' for i, x in
+               zip(range(len(bins_dist) - 1), ['Low', 'Medium', 'High'])]
 country_capitals = {'MX': 'Mexico City', 'BR': 'São Paulo', 'CO': 'Bogota',
                     'PH': 'Quezon City [Manila]', 'ID': 'Jakarta', 'ZA': 'Johannesburg', 'AR': 'GBuenos Aires'}
-
 
 ### Comute migration per socio economic group
 df_wl = pd.DataFrame()
 usr_df = pd.DataFrame()
 for country in countries:
     print(country)
-    out, usrs = compute_rural_migration_stats_city(country, bins_wealth, labels_wealth, hw, ww, wa, mph, mpw, activity_level, c_dates, admin_path, stop_path)
+    out, usrs = compute_rural_migration_stats_city(country, bins_wealth, labels_wealth, hw, ww, wa, mph, mpw,
+                                                   activity_level, c_dates, admin_path, stop_path)
     usr_df = pd.concat([usr_df, usrs])
     df_wl = pd.concat([df_wl, out], sort=True)
 
-df_wl.to_csv(results_dir+'df_wl_migration_weighted.csv')
-usr_df.to_csv(results_dir+'df_usr_migration_weighted.csv')
+df_wl.to_csv(results_dir + 'df_wl_migration_weighted.csv')
+usr_df.to_csv(results_dir + 'df_usr_migration_weighted.csv')
